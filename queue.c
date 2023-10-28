73ef3b94d9392004b945f8cd07ae58c5b40abe73
Jose Thomas <145546568+josethomas45@users.noreply.github.com>
2023-10-28 14:46:36 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,98 +1,102 @@
 #include<stdio.h>
-int stack[30],front=0,rear=-1,n,choice;
+int queue[30],front=0,rear=-1,n,choice;
 void enqueue();
 void dequeue();
 void peak();
 void display();
-
 int main()
 {
-	printf("Enter the number of terms: \n");
-	scanf("%d",&n);
-	printf("1) For enqueue \n2) For dequeue \n3) For peak \n4) To display \n5) To exit \n");
-
-do
-{
-	printf("Enter the choice:\t");
-	scanf("%d",&choice);
-	switch(choice)
-	{
-		case 1:
-		enqueue();
-		break;
-		
-		case 2:
-		dequeue();
-		break;
-		
-		case 3:
-		peak();
-		break;
-		
-		case 4:
-		display();
-		break;
-		
-		case 5:
-		break;
-		
-		default:
-		break;
-	}
-	}while(choice!=5);
+   printf("Enter the number of terms:\n");
+   scanf("%d",&n);
+   
+   do
+   {
+      printf("1) to enqueue\t2) to dequeue\t3) peak\t4) to display the stack\t5) to exit \t");
+      printf("Enter a choie:\n");
+      scanf("%d",&choice);
+      switch(choice)
+      {
+         case 1:
+         enqueue();
+         break;
+         
+         case 2:
+         dequeue();
+         break;
+         
+         case 3:
+         peak();
+         break;
+         
+         case 4:
+         display();
+         break;zqss
+         
+         case 5:
+         break;
+         
+         default:
+         printf("Enter a valid choice\n");
+         break;
+      }
+   }while(choice!=5);
 }
-
-
-	void enqueue()
-	{
-		if(rear<n-1)
-		{
-			rear=rear+1;
-			printf("Enter the element to be enqueue: \n");
-			scanf("%d",&stack[rear]);
-		}
-		else
-		{
-			printf("The stack is OVERFLOW situation \n");
-		}
-	}
-
-	void dequeue()
-	{
-		if(rear<front)
-		{
-			printf("The stack is in UNDERFLOW situation \n");
-		}
-		else
-		{
-			printf("The dequeued element is %d \n",stack[front]);
-			front=front+1;
-		}
-	}
-
-	void peak()
-	{
-		int i;
-		if(rear<front){
-		printf("The stack is empty \n");
-		}
-		else{
-		printf("The front element is %d \n",stack[front]);
-		}
-	}
-	
-	void display()
-	{
-		int i;
-		if(rear<front){
-		printf("The stack is empty \n");
-		}
-		else{
-		printf("The element in the stack are: \n");
-		for(i=rear;i>=front;i--)
-		{
-			printf("%d \n", stack[i]);
-		}
-	}
-	
-}
+   
+   void enqueue()
+   {
+     if(rear<n-1)
+     {
+        rear=rear+1;
+        printf("Enter the element to enqueue:\n");
+        scanf("%d",&queue[rear]);  
+     }
+     else
+     {
+        printf("The queue is in OVERFLOW condition\n");
+     }
+   }
+   
+   void dequeue()
+   {   if(front==rear){
+       printf("The dequeued element is %d\n",queue[front]);
+       front=0;
+       rear=-1;
+   }
+       else if(rear<front)
+       {
+          printf("The queue is in UNDERFLOW condition\n");
+          
+       }
+       else
+       {
+          printf("The dequeued element is %d\n",queue[front]);
+          front=front+1;;
+       }
+   }
+   
+   void peak()
+   {
+   if(rear<front){
+      printf("The queue is empty\n");
+   }
+   else
+   {
+     printf("The frontmost element is %d\n",queue[front]);
+     }
+   }
+   
+   void display()
+   {
+     int i;
+     if(rear<front){
+      printf("The queue is empty\n");
+   }
+   else
+   {
+     printf("The elements in the queue are:\n");
+     for(i=rear;i>=front;i--)
+     {
+        printf("%d\n",queue[i]);
+     }
+     }
+   }
